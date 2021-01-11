5cc5b4164e3c5f3627c8f9c062ef96ed2143434c
Nelson Eldoro <nelsoneldoro@gmail.com>
2021-01-11 11:42:33 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # aspose-cells
+
+
+```
+// install java and aspose
+$ yarn
+// run hello word
+$ yarn dev
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+ var aspose = aspose || {};
+
+aspose.cells = require("aspose.cells");
+
+var workbook = new aspose.cells.Workbook(aspose.cells.FileFormatType.XLSX);
+
+workbook.getWorksheets().get(0).getCells().get("A1").putValue("testin...");
+
+workbook.save("out1.xlsx");
+
+console.log("file was created");
